70aa652d4b7011f238005ae51c75a87708538c66
Jordan Herd <jordan_herd@sfu.ca>
2026-07-19 19:15:56 -0700

    Added if statements around lock

--- lab8/src/lab8.c
+++ lab8/src/lab8.c
@@ -33,7 +33,8 @@ static void add_word_counts_in_chunk(count_map_t *map, word_t *words, size_t num
 
   for (size_t i = 0; i < num_words; i++) {
 
-    pthread_mutex_lock(lock);
+    if (lock)
+      pthread_mutex_lock(lock);
 
     word_count_entry_t *w = NULL;
     HASH_FIND_STR(*map, words[i], w);
@@ -45,7 +46,8 @@ static void add_word_counts_in_chunk(count_map_t *map, word_t *words, size_t num
       HASH_ADD_STR(*map, word, w);
     }
 
-    pthread_mutex_unlock(lock);
+    if (lock)
+      pthread_mutex_unlock(lock);
   }
 }
 
