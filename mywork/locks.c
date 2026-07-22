416f1e7fd2db8219f65d9e42440e85e4805b23d3
Jordan Herd <jordan_herd@sfu.ca>
2026-07-22 11:54:56 -0700

    .

---
+++ mywork/locks.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static pthread_mutex_t mutex0 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t another_lock = PTHREAD_MUTEX_INITIALIZER;
+
+static void *thread0(void *arg) {
+  pthread_mutex_lock(&another_lock);
+  {
+    pthread_mutex_lock(&mutex0);
+    printf("thread0: mutex0\n");
+    pthread_mutex_lock(&mutex1);
+  }
+
+  pthread_mutex_unlock(&another_lock);
+  printf("thread 0 mutex1\n");
+  pthread_mutex_unlock(&mutex1);
+  pthread_mutex_unlock(&mutex0);
+  pthread_exit(0);
+}
+
+static void *thread1(void *arg) {
+  pthread_mutex_lock(&another_lock);
+  {
+    pthread_mutex_lock(&mutex1);
+    printf("thread1: mutex1\n");
+    pthread_mutex_lock(&mutex0);
+  }
+
+  pthread_mutex_unlock(&another_lock);
+  printf("thread 1 mutex0\n");
+  pthread_mutex_unlock(&mutex0);
+  pthread_mutex_unlock(&mutex1);
+  pthread_exit(0);
+}
+
+int main() {
+
+  thread1();
+  thread2();
+}
