a60bb1a7b055b7cc75dabf7bcb07b188d4f56aec
Jordan Herd <jordan_herd@sfu.ca>
2026-08-12 15:27:25 -0700

    .'

---
+++ mywork/philosophers.c
@@ -0,0 +1,38 @@
+
+#define NUMBER 5
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+static pthread_mutex_t mtx[NUMBER] = {PTHREAD_MUTEX_INITIALIZER};
+
+static void *thread_func(void *arg) {
+  int left = (int)arg;
+  int right = ((int)arg + 1) % NUMBER;
+  for (;;) {
+    printf("Thread %d: thinking\n", (int)arg);
+    sleep(5);
+    pthread_mutex_lock(&mtx[left]);
+    if (pthread_mutex_trylock(&mtx[right]) != 0) {
+      pthread_mutex_unlock(&mtx[left]);
+      continue;
+    }
+    printf("Thread %d: eating\n", (int)arg);
+    pthread_mutex_unlock(&mtx[left]);
+    pthread_mutex_unlock(&mtx[right]);
+  }
+  return 0;
+}
+
+int main() {
+  pthread_t t[NUMBER];
+  for (int i = 0; i < NUMBER; ++i) {
+
+    printf("i %d\n", i);
+    pthread_create(&t[i], NULL, thread_func, &i);
+  }
+  for (int i = 0; i < NUMBER; ++i) {
+    pthread_join(t[i], NULL);
+  }
+}
