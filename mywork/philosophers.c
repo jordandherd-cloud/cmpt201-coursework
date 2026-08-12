dde351e1993f54edd6f82bada5b86dd9023e426c
Jordan Herd <jordan_herd@sfu.ca>
2026-08-12 15:34:05 -0700

    .

--- mywork/philosophers.c
+++ mywork/philosophers.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 
-static pthread_mutex_t mtx[NUMBER] = {PTHREAD_MUTEX_INITIALIZER};
+static pthread_mutex_t mtx[NUMBER];
 
 static void *thread_func(void *arg) {
   int left = (int)arg;
@@ -27,10 +27,16 @@ static void *thread_func(void *arg) {
 
 int main() {
   pthread_t t[NUMBER];
+  int ids[NUMBER];
+  for (int i = 0; i < NUMBER; i++) {
+    pthread_mutex_init(&mtx[i], NULL);
+  }
   for (int i = 0; i < NUMBER; ++i) {
 
+    ids[i] = i;
+
     printf("i %d\n", i);
-    pthread_create(&t[i], NULL, thread_func, &i);
+    pthread_create(&t[i], NULL, thread_func, &ids[i]);
   }
   for (int i = 0; i < NUMBER; ++i) {
     pthread_join(t[i], NULL);
