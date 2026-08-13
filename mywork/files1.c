5611816c236da91337d8936b260f926bc3efb0d1
Jordan Herd <jordan_herd@sfu.ca>
2026-08-13 16:46:32 -0700

    Studying files

--- mywork/files1.c
+++ mywork/files1.c
@@ -8,39 +8,42 @@
 //
 int main() {
 
-  char buf[1024];
+  /*  char buf[1024];
 
-  FILE *tmp;
+    FILE *tmp;
 
-  tmp = fopen("tmp", O_CREAT);
+    tmp = fopen("tmp", "w+");
 
-  if (tmp == NULL)
-    printf("File not opened\n");
+    if (tmp == NULL)
+      printf("File not opened\n");
 
-  fprintf(tmp, "Hello World!");
+    fprintf(tmp, "Hello World!");
 
-  fseek(tmp, 6, 0);
+    fseek(tmp, -6, SEEK_CUR);
 
-  fclose(tmp);
-  return 0;
+    fread(buf, 1, 6, tmp);
+    buf[6] = '\0';
+    printf("%s\n", buf);
+    fclose(tmp);
+    return 0;*/
+
+  char *str = "hello world";
+  char buf[12];
+
+  int fd = open("tmp", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+  if (fd == -1) {
+    perror("Open failed");
+
+    exit(0);
+  }
+
+  write(fd, str, strlen(str));
+
+  lseek(fd, -6, SEEK_CUR);
+  read(fd, buf, 6);
+  write(STDOUT_FILENO, buf, 6);
+  close(fd);
+
+  printf("\n");
 }
-
-char *str = "hello world";
-char buf[12];
-
-int fd = open("tmp", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-
-if (fd == -1) {
-  perror("Open failed");
-
-  exit(SYSTEM_FAILED);
-}
-
-write(fd, str, strlen(str));
-
-lseek(fd, -6, SEEK_CUR);
-read(fd, buf, 6);
-write(STDOUT_FILENO, buf, 6);
-close(fd);
-
-printf("\n");
